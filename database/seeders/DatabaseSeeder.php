33fd04ef4eaab56c36f64e612cccef2c74ff682d
Sudarsan999 <srisudarsanperla@gmail.com>
2023-11-20 15:00:10 +0530

    first

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+
+
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('users')->insert([
+        
+            'name' => 'MD.Admin',
+            'email_verified_at' => new DATETIME(),
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'remember_token' => str::random(10),
+            'created_at' => new DATETIME(),
+            'updated_at' => new DATETIME(),
+
+
+        ]);
+        
+    }
+}
